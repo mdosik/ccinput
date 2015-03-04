a54cf7d36a32c289f9194aef558be37e6fdbe21a
mdosik <mikedosik@yahoo.com>
2015-03-03 21:54:26 -0800

    ooh... I made a modal box come up

--- ccinput.js
+++ ccinput.js
@@ -1,6 +1,6 @@
 /*global angular*/
 
-var ccinputApp = angular.module('ccinputApp',['ngRoute']);
+var ccinputApp = angular.module('ccinputApp',['ngRoute','ui.bootstrap']);
 
 ccinputApp.config(function ($routeProvider) {
     'use strict';
@@ -11,8 +11,8 @@ ccinputApp.config(function ($routeProvider) {
             controller  : 'mainController'
         })
         .when('/ccinput', {
-            templateUrl : 'pages/ccinput.html',
-            controller  : 'ccinputController'
+            templateUrl : 'pages/ccinput.html',    
+            controller  : 'ccinputModalController'
         });
 });
 
@@ -20,6 +20,16 @@ ccinputApp.config(function ($routeProvider) {
 ccinputApp.controller('mainController', ['$scope',function($scope) {
 }]);
 
-ccinputApp.controller('ccinputController', ['$scope',function($scope) {
+ccinputApp.controller('ccinputModalController', ['$scope','$modal','$log',function($scope, $modal, $log) {
+    
+    $scope.open = function () {
+        var modalInstance = $modal.open({
+            templateUrl : 'ccinputModalContent.html',
+            controller  : 'ccinputModalInstanceControl'
+        });
+    };
+}]);
+
+ccinputApp.controller("ccinputModalInstanceControl", ['$scope',function($scope) {
 }]);
 
